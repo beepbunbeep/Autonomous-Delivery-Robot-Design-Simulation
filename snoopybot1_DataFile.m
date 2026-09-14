b89a4c529d0e72c2ece75e1d1a049c0a3135953f
beepbunbeep <beepbunbeep@proton.me>
2026-09-14 22:06:53 +0200

    Add files via upload

---
+++ snoopybot1_DataFile.m
@@ -0,0 +1,154 @@
+% Simscape(TM) Multibody(TM) version: 26.1%%%%% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+%============= RigidTransform%
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(13).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(13).angle = 0.0;
+smiData.RigidTransform(13).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(13).ID = "";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [179.20650752144635 30 -290.00000000000006];  % mm
+smiData.RigidTransform(1).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(1).axis = [1 0 0];
+smiData.RigidTransform(1).ID = "B[snoopy_house-1:-:snoopy!wheel-2]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [-1.9673151996357774e-13 20.000000000000004 2.8421709430404007e-14];  % mm
+smiData.RigidTransform(2).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(2).axis = [0.57735026918962584 -0.57735026918962573 0.57735026918962573];
+smiData.RigidTransform(2).ID = "F[snoopy_house-1:-:snoopy!wheel-2]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [179.20650752144635 30 290.00000000000006];  % mm
+smiData.RigidTransform(3).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(3).axis = [1 0 0];
+smiData.RigidTransform(3).ID = "B[snoopy_house-1:-:snoopy!wheel-3]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [1.7047474543119279e-13 19.999999999999996 3.5527136788005009e-15];  % mm
+smiData.RigidTransform(4).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(4).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(4).ID = "F[snoopy_house-1:-:snoopy!wheel-3]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [-175 30 291.84694222040548];  % mm
+smiData.RigidTransform(5).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(5).axis = [1 0 0];
+smiData.RigidTransform(5).ID = "B[snoopy_house-1:-:snoopy!wheel-4]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [1.7047474543119279e-13 20 7.1054273576010019e-15];  % mm
+smiData.RigidTransform(6).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(6).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(6).ID = "F[snoopy_house-1:-:snoopy!wheel-4]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [-175 30 -291.84694222040548];  % mm
+smiData.RigidTransform(7).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(7).axis = [1 0 0];
+smiData.RigidTransform(7).ID = "B[snoopy_house-1:-:snoopy!wheel-5]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-1.6897594434794883e-13 20 2.4868995751603507e-14];  % mm
+smiData.RigidTransform(8).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(8).axis = [0.57735026918962584 -0.57735026918962573 0.57735026918962573];
+smiData.RigidTransform(8).ID = "F[snoopy_house-1:-:snoopy!wheel-5]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [-300.00000000000006 15.000000000000014 260.00000000000006];  % mm
+smiData.RigidTransform(9).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(9).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(9).ID = "B[snoopy!-1:-:snoopy_drawer-2]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [-481.70886171665381 0 260.00000000000006];  % mm
+smiData.RigidTransform(10).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(10).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(10).ID = "F[snoopy!-1:-:snoopy_drawer-2]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [-304.99999999999989 450 230.00000000000003];  % mm
+smiData.RigidTransform(11).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(11).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(11).ID = "B[snoopy!-1:-:snoopy_house-1]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [-304.99999999999989 530 230.00000000000003];  % mm
+smiData.RigidTransform(12).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(12).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(12).ID = "F[snoopy!-1:-:snoopy_house-1]";
+
+%%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [0 0 0];  % mm
+smiData.RigidTransform(13).angle = 0;  % rad
+smiData.RigidTransform(13).axis = [0 0 0];
+smiData.RigidTransform(13).ID = "RootGround[snoopy!-1]";
+
+%============= Solid%
+%%%Product of Inertia (PoI)
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(4).mass = 0.0;
+smiData.Solid(4).CoM = [0.0 0.0 0.0];
+smiData.Solid(4).MoI = [0.0 0.0 0.0];
+smiData.Solid(4).PoI = [0.0 0.0 0.0];
+smiData.Solid(4).color = [0.0 0.0 0.0];
+smiData.Solid(4).opacity = 0.0;
+smiData.Solid(4).ID = "";
+
+%%Visual Properties - Simple
+smiData.Solid(1).mass = 13.888070987450366;  % kg
+smiData.Solid(1).CoM = [6.9489720284644401 29.655506035287807 -1.4680571749308657];  % mm
+smiData.Solid(1).MoI = [330274.36843339744 782890.74431331991 467550.40941304434];  % kg*mm^2
+smiData.Solid(1).PoI = [368.5608573894371 -5541.8439394961279 -1034.7484121295599];  % kg*mm^2
+smiData.Solid(1).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = "snoopy_house*:*Default";
+
+%%Visual Properties - Simple
+smiData.Solid(2).mass = 11.270057530513107;  % kg
+smiData.Solid(2).CoM = [0 130.71748180837221 0];  % mm
+smiData.Solid(2).MoI = [592462.20268692612 1045913.8425721317 763950.09857173276];  % kg*mm^2
+smiData.Solid(2).PoI = [0 0.055949302298969487 0];  % kg*mm^2
+smiData.Solid(2).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = "snoopy_drawer*:*Default";
+
+%%Visual Properties - Simple
+smiData.Solid(3).mass = 0.35544160282905479;  % kg
+smiData.Solid(3).CoM = [0 11.23540593877453 0];  % mm
+smiData.Solid(3).MoI = [467.10247755511887 896.84212170915544 467.10247755511881];  % kg*mm^2
+smiData.Solid(3).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(3).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = "snoopy!wheel*:*Default";
+
+%%Visual Properties - Simple
+smiData.Solid(4).mass = 58.762867082123755;  % kg
+smiData.Solid(4).CoM = [-12.591202390075647 336.67981364355893 8.4089125760806098];  % mm
+smiData.Solid(4).MoI = [4460355.6406405754 5471242.1838295879 6453241.5938377874];  % kg*mm^2
+smiData.Solid(4).PoI = [-118522.66829800449 -111511.3461008875 -1344983.4659628896];  % kg*mm^2
+smiData.Solid(4).color = [1 1 1];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = "snoopy!*:*Default";
+
+%============= Joint%
+%%%%%%%%%%Position Target (Pos)
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(4).Rz.Pos = 0.0;
+smiData.RevoluteJoint(4).ID = "";
+
+smiData.RevoluteJoint(1).Rz.Pos = 96.767729062381804;  % deg
+smiData.RevoluteJoint(1).ID = "[snoopy_house-1:-:snoopy!wheel-2]";
+
+smiData.RevoluteJoint(2).Rz.Pos = 91.31537495649988;  % deg
+smiData.RevoluteJoint(2).ID = "[snoopy_house-1:-:snoopy!wheel-3]";
+
+smiData.RevoluteJoint(3).Rz.Pos = 91.895148756495232;  % deg
+smiData.RevoluteJoint(3).ID = "[snoopy_house-1:-:snoopy!wheel-4]";
+
+smiData.RevoluteJoint(4).Rz.Pos = 97.301051132817463;  % deg
+smiData.RevoluteJoint(4).ID = "[snoopy_house-1:-:snoopy!wheel-5]";
+
